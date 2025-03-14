86b51b1224b2e4438c99f2cac8a086eaf78be369
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:07:38 +0530

    Update variables.tf

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,1 +1,21 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+}
+
+variable "public_subnet_cidr" {
+  description = "Public Subnet CIDR"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+}
 
