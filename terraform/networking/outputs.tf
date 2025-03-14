50cbf26b526c2711dcddcc6ec09021e6147bab47
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:15:52 +0530

    Update outputs.tf

--- terraform/networking/outputs.tf
+++ terraform/networking/outputs.tf
@@ -1,1 +1,20 @@
+output "aws_vpc_peering_id" {
+  value = aws_vpc_peering_connection.aws_to_gcp.id
+}
+
+output "gcp_vpc_peering_name" {
+  value = google_compute_network_peering.gcp_to_aws.name
+}
+
+output "azure_vnet_peering_id" {
+  value = azurerm_virtual_network_peering.azure_to_gcp.id
+}
+
+output "aws_vpn_connection_id" {
+  value = aws_vpn_connection.aws_to_gcp.id
+}
+
+output "gcp_vpn_tunnel_name" {
+  value = google_compute_vpn_tunnel.gcp_to_aws.name
+}
 
