3489d9a115e3afe5d8777a58c37add76a20914c0
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:28:01 +0530

    Update restore.sh

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+echo "Starting restore process..."
+
+# AWS Restore
+aws s3 sync s3://my-backup-bucket /data
+
+# GCP Restore
+gsutil -m rsync -r gs://my-backup-bucket /data
+
+# Azure Restore
+az storage blob download-batch -d /data --account-name mystorageaccount -s my-container
+
+echo "Restore completed!"
 
