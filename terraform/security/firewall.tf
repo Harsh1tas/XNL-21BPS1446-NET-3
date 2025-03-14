ad855ddaba440ab26d769a6ad60cbd928a1c72c0
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:20:14 +0530

    Update firewall.tf

--- terraform/security/firewall.tf
+++ terraform/security/firewall.tf
@@ -1,1 +1,38 @@
+resource "aws_security_group" "security_group" {
+  name        = "SecurityGroup"
+  description = "Security group with limited access"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow SSH Access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.firewall_cidr]
+  }
+
+  # Allow HTTP Access
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.firewall_cidr]
+  }
+
+  # Allow HTTPS Access
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.firewall_cidr]
+  }
+
+  # Outbound Rules (Allow All)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
