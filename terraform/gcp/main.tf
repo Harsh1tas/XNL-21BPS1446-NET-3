ba628a33447026182321df3c5945bf0ab7209de5
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:08:22 +0530

    Update main.tf

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,1 +1,21 @@
+provider "google" {
+  project = var.project_id
+  region  = var.gcp_region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "gcp-multicloud-vpc"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "gcp-instance"
+  machine_type = var.machine_type
+  zone         = var.gcp_zone
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+}
 
