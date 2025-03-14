841c5717f98a2adcc86914e66f1b785ab99c1866
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:19:30 +0530

    Update output.tf

--- terraform/security/output.tf
+++ terraform/security/output.tf
@@ -1,1 +1,16 @@
 
+output "iam_role_arn" {
+  description = "IAM Role ARN"
+  value       = aws_iam_role.security_role.arn
+}
+
+output "firewall_id" {
+  description = "Firewall/Security Group ID"
+  value       = aws_security_group.security_group.id
+}
+
+output "waf_arn" {
+  description = "WAF Web ACL ARN"
+  value       = aws_wafv2_web_acl.waf_acl.arn
+}
+
