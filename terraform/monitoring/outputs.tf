49a38d283f33ce9846ed1a008e4318f4ea237e05
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:22:02 +0530

    Update outputs.tf

--- terraform/monitoring/outputs.tf
+++ terraform/monitoring/outputs.tf
@@ -1,1 +1,15 @@
+output "cloudwatch_log_group_arn" {
+  description = "CloudWatch Log Group ARN"
+  value       = aws_cloudwatch_log_group.main.arn
+}
+
+output "gcp_logging_project_id" {
+  description = "GCP Logging Project ID"
+  value       = google_logging_project_sink.main.project
+}
+
+output "azure_monitor_workspace_id" {
+  description = "Azure Monitor Log Analytics Workspace ID"
+  value       = azurerm_log_analytics_workspace.monitor.id
+}
 
