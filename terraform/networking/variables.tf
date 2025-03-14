2fe0b38874466217719064f9b17e0f66b18570d5
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:14:52 +0530

    Update variables.tf

--- terraform/networking/variables.tf
+++ terraform/networking/variables.tf
@@ -1,1 +1,30 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "gcp_project_id" {
+  description = "GCP project ID"
+}
+
+variable "aws_vpc_id" {
+  description = "AWS VPC ID"
+}
+
+variable "gcp_vpc_id" {
+  description = "GCP VPC ID"
+}
+
+variable "azure_vnet_id" {
+  description = "Azure Virtual Network ID"
+}
+
+variable "aws_vpn_ip" {
+  description = "AWS VPN Gateway Public IP"
+}
+
+variable "vpn_shared_secret" {
+  description = "Shared secret for VPN tunnels"
+  sensitive   = true
+}
 
