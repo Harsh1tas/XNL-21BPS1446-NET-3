14f64e35452c7e9d7ba22911637d1d219b06c3d5
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:22:16 +0530

    Update variables.tf

--- terraform/monitoring/variables.tf
+++ terraform/monitoring/variables.tf
@@ -1,1 +1,23 @@
 
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "gcp_project_id" {
+  description = "GCP Project ID for Logging"
+  type        = string
+}
+
+variable "azure_resource_group" {
+  description = "Azure Resource Group Name"
+  type        = string
+}
+
+variable "log_retention_days" {
+  description = "Log retention period in days"
+  type        = number
+  default     = 30
+}
+
