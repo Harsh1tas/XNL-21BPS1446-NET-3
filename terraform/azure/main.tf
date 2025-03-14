de7109cd48415b40bdc0b0b770ad02515b7e6c7f
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:09:09 +0530

    Update main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,1 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "AzureMultiCloudRG"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Azure-VNet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = [var.vnet_cidr]
+}
 
