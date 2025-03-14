91234b00314d28973ec88a4b4297dfecec25b0d1
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:22:41 +0530

    Update stackdriver.tf

--- terraform/monitoring/stackdriver.tf
+++ terraform/monitoring/stackdriver.tf
@@ -1,1 +1,11 @@
+resource "google_logging_project_sink" "main" {
+  name        = "gcp-log-sink"
+  destination = "storage.googleapis.com/${google_storage_bucket.logs.name}"
+  filter      = "resource.type=gce_instance AND severity>=WARNING"
+}
+
+resource "google_storage_bucket" "logs" {
+  name     = "gcp-log-bucket"
+  location = "US"
+}
 
