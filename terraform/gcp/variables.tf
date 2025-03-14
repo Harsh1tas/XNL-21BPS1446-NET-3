2c6823588418ffb858a0abe55d875bd5866d31fe
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:08:37 +0530

    Update variables.tf

--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -1,1 +1,20 @@
+variable "project_id" {
+  description = "GCP Project ID"
+}
+
+variable "gcp_region" {
+  description = "GCP Region"
+}
+
+variable "gcp_zone" {
+  description = "GCP Zone"
+}
+
+variable "machine_type" {
+  description = "GCP VM Instance Type"
+}
+
+variable "image" {
+  description = "GCP Image for VM"
+}
 
