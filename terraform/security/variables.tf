7655a56a1404d334bbdaeea9d5dd5796cda30691
Harshita Singh <159155427+Harsh1tas@users.noreply.github.com>
2025-03-14 18:19:47 +0530

    Update variables.tf

--- terraform/security/variables.tf
+++ terraform/security/variables.tf
@@ -1,1 +1,23 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "iam_user_name" {
+  description = "IAM User Name"
+  type        = string
+}
+
+variable "firewall_cidr" {
+  description = "CIDR block for security group"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "waf_rule_priority" {
+  description = "Priority of WAF rule"
+  type        = number
+  default     = 1
+}
 
